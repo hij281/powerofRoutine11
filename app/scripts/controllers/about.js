232388656cb0b227284d689db2e0efa5626982c1
hij <ijhwang@azsoft.kr>
2018-02-07 22:01:46 +0900

    최초일괄등록 - 황인준

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name powerofRoutineApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the powerofRoutineApp
+ */
+angular.module('powerofRoutineApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name powerofRoutineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the powerofRoutineApp
+ */
+angular.module('powerofRoutineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
